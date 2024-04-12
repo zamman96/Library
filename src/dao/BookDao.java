90c77b59defd3e7f2048a87cf8ee49e484f5d672
PC-13 <PC-13@DESKTOP-062MQF6>
2024-04-12 14:55:30 +0900

    commit

--- src/dao/BookDao.java
+++ src/dao/BookDao.java
@@ -1,5 +1,21 @@
 package dao;
 
+/**
+ * @author 송예진
+ *
+ */
 public class BookDao {
+	private static BookDao instance;
 
+	private BookDao() {
+
+	}
+
+	public static BookDao getInstance() {
+		if (instance == null) {
+			instance = new BookDao();
+		}
+		return instance;
+	}
+	
 }
--- src/service/BookService.java
+++ src/service/BookService.java
@@ -1,5 +1,22 @@
 package service;
 
+/**
+ * @author 송예진
+ *
+ */
 public class BookService {
+	private static BookService instance;
+
+	private BookService() {
+
+	}
+
+	public static BookService getInstance() {
+		if (instance == null) {
+			instance = new BookService();
+		}
+		return instance;
+	}
+	
 
 }
